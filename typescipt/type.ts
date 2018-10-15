79304463ed76cce4c27302e326fdff1754b2d5ef
nieyao <1161781399@qq.com>
2018-10-15 22:41:37 +0800

    typescript

---
+++ typescipt/type.ts
@@ -0,0 +1,20 @@
+let isDone:boolean = false;
+isDone =true;
+
+let decLiteral: number = 6;
+decLiteral = 7;
+let hexliteral:number = 0xf00d;
+let binaryLiteral:number = 0b1010;
+let octalLiteral:number = 0o744;
+
+
+//字符串
+let nickname:string = "boob";
+let age:number = 37;
+let sentence:string = `Hello,myname is ${nickname} I'll be ${age++} years old next month`;
+
+let list:number[] = [1,2,3];
+let list2:Array<number> = [1,2,3];
+
+let notSure:any =4;
+notSure = "ff";
---
+++ typescipt/tys.ts
@@ -0,0 +1,6 @@
+// function greeting(person:string){
+//   return "Hello," + person + "你最近又胖了";
+// }
+
+// var user:string = "吴冕";
+// document.body.innerHTML = greeting(user);
