5cc752705c1fc4c6d5d65b8d33736bbcac7559f9
nieyao <1161781399@qq.com>
2018-11-17 21:04:24 +0800

    wepy-shop

---
+++ wepy-shop/src/api/api.js
@@ -0,0 +1,13 @@
+// Application Interface
+// Server Web Application
+import {
+  wxRequest
+} from "@/utils/wxRequest";
+
+const apiMall = 'https://sujiefs.com';
+
+const getAdList = (params) =>  wxRequest(params, apiMall + '/api/adverts/list');
+
+export default {
+  getAdList
+}
