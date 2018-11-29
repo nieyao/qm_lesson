3222ca56c85440a3790a915dfb2eb77c943f7677
nieyao <1161781399@qq.com>
2018-11-29 14:48:43 +0800

    Hello React

---
+++ react/webpack-react-tutorial/readme.md
@@ -0,0 +1,15 @@
+SPA single Page Application
+#root  MVVM 组件式开发
+Component 
+只有一个，react 就是一个完全实践 MVVM 组件式开发的NO.1框架
+Component es6 modules 
+webpack 来做开发流程 src
+webpack 编译及分派工作
+webpack-cli 接受脚本，进行编译 dev
+
+react 相比于小程序 MVVM 上小程序抄的react
+不同的地方在于组件开发思想更加的专业
+- 容器组件
+  FormComponent 容器组件
+    InputComponent 表现式组件
+    InputComponent
---
+++ react/webpack-react-tutorial/src/js/components/presentational/input.js
@@ -0,0 +1,17 @@
+// react 组件有多种写法 容器Component+表现键头函数
+// props + render wxml 表现型组件 
+import React from 'react';
+const Input = ({ label, text, type, id, value, handleChange }) => (
+  <div className="form-group">
+    <label htmlFor={label}>{text}</label>
+    <input 
+      type={type}
+      className="form-control"
+      id={id}
+      value={value}
+      onChange={handleChange}
+      required
+    />
+  </div>
+);
+export default Input;
