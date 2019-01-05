fa9cd33755a74cdac7e50312ab3dd77bc195c0a5
nieyao <1161781399@qq.com>
2019-01-05 19:04:55 +0800

    容器式组件

---
+++ react/presentation-and-container/src/Clock/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Clock from './Clock'
+
+export default class ClockContainer extends React.Component {
+  constructor (props) {
+    super(props);
+    // time setInterval
+    this.state = {
+      time:props.time
+    }
+  }
+
+  componentDidMount () {
+    this._interval = setInterval(this._update, 1000)
+  }
+
+  _update = () => {
+    this.setState({
+      time: new Date(this.state.time.getTime() + 1000)
+    })
+  }
+
+  _exact = (time) => {
+    return {
+      hours: time.getHours(),
+      minutes: time.getMinutes(),
+      seconds: time.getSeconds()
+    }
+  }
+
+  render () {
+    return <Clock {...this._exact(this.state.time)} />
+  }
+}
+
+ClockContainer.propTypes = {
+  time: PropTypes.object.isRequired
+}
