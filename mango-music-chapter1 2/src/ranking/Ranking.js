204dcb57f5d59648df025a8906b329929476ad76
nieyao <1161781399@qq.com>
2019-01-27 10:12:12 +0800

    mango-music

---
+++ mango-music-chapter1 2/src/ranking/Ranking.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './ranking.styl'; 
+
+
+class Ranking extends Component {
+
+  render() {
+    return (
+      <div>Ranking</div>
+    )
+  }
+}
+export default Ranking;
