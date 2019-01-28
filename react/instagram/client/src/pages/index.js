74b0000f4f764d220c56e7d8716d2ddd84b9b081
nieyao <1161781399@qq.com>
2019-01-28 21:49:37 +0800

    nav 组件

---
+++ react/instagram/client/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from "@components/nav/index.js";
+
+class Instagram extends React.Component {
+  render () {
+    return (
+      <main>
+        <Nav />
+      </main>
+    )
+  }
+
+}
+
+export default Instagram;
